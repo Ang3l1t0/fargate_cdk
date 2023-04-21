ed39dae58c766b1a87776f8ad672e5845258091f
Ang3l1t0 <aepalma4712@gmail.com>
2023-04-20 20:03:39 -0500

    first commit

--- fargate_cdk/fargate_cdk_stack.py
+++ fargate_cdk/fargate_cdk_stack.py
@@ -1,11 +1,8 @@
 from constructs import Construct
-from aws_cdk import (
-    Duration,
+from aws_cdk.core import (
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_ec2 as ec2,
+    Tags,
 )
 
 
@@ -14,13 +11,42 @@ class FargateCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "FargateCdkQueue",
-            visibility_timeout=Duration.seconds(300),
+        # Create a VPC with CIDR block 10.0.0.0/16 0/16
+        vpc = ec2.Vpc(
+            self, "StgVpc",
+            cidr="10.0.0.0/16",
+            max_azs=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public-1a",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                ),
+                ec2.SubnetConfiguration(
+                    name="public-1b",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                ),
+                ec2.SubnetConfiguration(
+                    name="public-1c",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                ),
+                ec2.SubnetConfiguration(
+                    name="public-1d",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                ),
+            ]
         )
 
-        topic = sns.Topic(
-            self, "FargateCdkTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # add tags to the VPC
+        Tags.of(vpc).add("Name", "StgVpc")
+        Tags.of(vpc).add("Environment", "Staging")
+        Tags.of(vpc).add("Owner", "Angel")
+        
+        # add tas to the subnets
+        for subnet in vpc.public_subnets:
+            Tags.of(subnet).add("Name", "StgPublicSubnet")
+            Tags.of(subnet).add("Environment", "Staging")
+            Tags.of(subnet).add("Owner", "Angel")
